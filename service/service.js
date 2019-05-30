5637787c7134a6f70d0474fb640463fb12f4b266
panupong <panupong043@gmail.com>
2019-05-30 10:21:03 +0700

    fix service change name

--- service/service.js
+++ service/service.js
@@ -12,36 +12,38 @@ Mongoose.connect('mongodb://localhost/mydb');
 app.use(BodyParser.json());
 app.use(BodyParser.urlencoded({ extended: true }));
 
-const PersonModel = Mongoose.model('movies', {
+const Movies = Mongoose.model('movies', {
   name: String,
   detail: String,
+  price: Number,
+  picture: String,
 });
 
 app.post('/movies', async (request, response) => {
-  const result = await new PersonModel(request.body).save();
+  const result = await new Movies(request.body).save();
   response.send(result);
 });
 
 app.get('/movies', async (request, response) => {
-  const result = await PersonModel.find().exec();
+  const result = await Movies.find().exec();
   response.send(result);
 });
 
 app.get('/movies/:id', async (request, response) => {
-  const person = await PersonModel.findById(request.params.id).exec();
+  const person = await Movies.findById(request.params.id).exec();
   response.send(person);
   console.log(person);
 });
 
 app.put('/movies/:id', async (request, response) => {
-  const person = await PersonModel.findById(request.params.id).exec();
+  const person = await Movies.findById(request.params.id).exec();
   person.set(request.body);
   const result = await person.save();
   response.send(result);
 });
 
 app.delete('/movies/:id', async (request, response) => {
-  const result = await PersonModel.deleteOne({ firstname: request.params.id }).exec();
+  const result = await Movies.deleteOne({ name: request.params.id }).exec();
   response.send(result);
 });
 
